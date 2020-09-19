cc047d6693f60ddc176f38983db71d96e0057c49
Jaehyeokk <lionjk92@naver.com>
2020-09-19 20:52:43 +0900

    renewal profile page

--- js/main.js
+++ js/main.js
@@ -1,83 +1,97 @@
-$(window).load(function(){
-  resizeEvent();
-  headerEffect();
-  workEffect();
+$(document).ready(function(){
+  visualEffect();
+  scrollEvent();
 })
-function resizeEvent(){
-  // work
+
+
+function visualEffect(){
+
+  visualHeight()
+  textEffect()
+
+  $(window).on('resize',visualHeight)
+  function visualHeight(){
+    $('.visual').css({'height':$(window).innerHeight()})
+  }
+  function textEffect(){
+    var $title0=$('.visual .text-wrap .title').eq(0);
+    var $title1=$('.visual .text-wrap .title').eq(1);
+    var $title2=$('.visual .text-wrap .title').eq(2);
+    var title1Num=$title1.children().size()
+    var title2Num=$title2.children().size()
   
-  visualResize();
-  workListResize();
+    init()
 
-  $(window).on('resize',resize);
+    function init(){
+      $title0.css({'opacity':0, 'top':-100})
+      $title1.children().css({"opacity":0,"top":-200})
+      $title2.children().css({"opacity":0,"top":200})
+    }
 
-  function resize(){
-    visualResize();
-    workListResize();
+    $title0.animate({'opacity':1, 'top':0},500,"easeOutCubic",function(){
+      for(var i=0; i<title1Num; i++){
+        $title1.children().eq(i).animate({"opacity":1,"top":0},500+(120*i),"easeOutCubic",function(){
+          for(var i=0; i<title2Num; i++){
+            $title2.children().eq(i).animate({"opacity":1,"top":0},500+(120*i),"easeOutCubic")
+          }
+        })
+      }
+    })
   }
-
-  function visualResize(){
-    $('#visual').css({'height':$('#visual>img').innerHeight()})
-  }
-  function workListResize(){
-    $('.work_list').css({'height':$('.work_list li img').innerHeight()+3})
-  }
-
 }
-function headerEffect(){
-  var $header=$('#header');
-  var $mainMenu=$('.mainmenu_list>li>a')
-  var menuIndex;
-  var menuTop=[];
-  menuTop[0]=$('#visual').offset().top-0;
-  menuTop[1]=$('#about').offset().top-200;
-  menuTop[2]=$('#skill').offset().top-200;
-  menuTop[3]=$('#work').offset().top-200;
 
-  init();
-  initEvent();
-  
-  function init(){
-    $header.css({'height':$(window).innerHeight()});
-    $mainMenu.parent('li').eq(0).addClass('selected');
-  }
+function scrollEvent(){
+  // scroll value
+  var currentScroll=$(document).scrollTop();
+  var newScroll;
+  var sectionTop=[];
+  sectionTop[0]=$('.about').offset().top;
+  sectionTop[1]=$('.work').offset().top;
+  sectionTop[2]=$('.contact').offset().top+500;
+
+  // header
+  var $mainMenu=$('.main-menu>li>a');
+  // visual
+  var $scrollDownBtn=$('.scroll-down');
+
+  initEvent()
+
   function initEvent(){
-    $(window).on('resize',init);
     $(window).on('scroll',onScroll);
+    $mainMenu.on('click',clickMenuScroll);
+    $scrollDownBtn.on('click',function(){
+      $('body,html').animate({scrollTop:sectionTop[0]},500,"easeOutCubic")
+    })
+  }
+
+  function clickMenuScroll(){
+    var menuNum=$mainMenu.index($(this));
+    $('body,html').animate({scrollTop:sectionTop[menuNum]},500,"easeOutCubic")
   }
 
   function onScroll(){
-    var scrollTop=$(document).scrollTop();
-    if(scrollTop<=menuTop[1]){
-      menuIndex=0;
-    }else if(scrollTop>=menuTop[1] && scrollTop<menuTop[2]){
-      menuIndex=1;
-    }else if(scrollTop>=menuTop[2] && scrollTop<menuTop[3]){
-      menuIndex=2;
-    }else if(scrollTop>=menuTop[3]){
-      menuIndex=3;
+    newScroll=$(document).scrollTop()
+
+    if(newScroll-currentScroll>0 || newScroll<sectionTop[0]-80){
+      $('header.on').filter(':not(:animated)').animate({'height':0},500,"easeOutCubic",function(){
+        $('header').removeClass('on')
+      })
+    }else if(newScroll-currentScroll<0 && newScroll>sectionTop[0]-80){
+      $('header').addClass('on');
+      $('header.on').filter(':not(:animated)').animate({'height':80},500,"easeOutCubic");
     }
-    $mainMenu.parent('li').removeClass('selected');
-    $mainMenu.parent('li').eq(menuIndex).addClass('selected');
-  }
-}
-function workEffect(){
-  var $workList=$('.work_list').children();
-  var $activeWorkList=$('.active_work_list').children();
-  var clickNum;
-
-  loadWork(0);
-
-  $workList.on('click',clickWork);
-
-  function clickWork(){
-    clickNum=$workList.index($(this));
-    loadWork(clickNum);
-  }
-  function loadWork(index){
-    $workList.removeClass('selected');
-    $workList.eq(index).addClass('selected');
-    $activeWorkList.css({'display':'none'})    
-    $activeWorkList.eq(index).css({'display':'block'})
+    if(newScroll<sectionTop[0]){
+      $mainMenu.parent().removeClass('on')
+    }else if(newScroll>sectionTop[0] && newScroll<sectionTop[1]){
+      $mainMenu.parent().removeClass('on')
+      $mainMenu.parent().eq(0).addClass('on')
+    }else if(newScroll>sectionTop[1] && newScroll<sectionTop[2]-500){
+      $mainMenu.parent().removeClass('on')
+      $mainMenu.parent().eq(1).addClass('on')
+    }else if(newScroll>sectionTop[2]-500){
+      $mainMenu.parent().removeClass('on')
+      $mainMenu.parent().eq(2).addClass('on')
+    }
+    currentScroll=newScroll
   }
 }
